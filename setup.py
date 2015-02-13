f989c3c14723e6d649e9bc08fe7d5a329752b718
Jan-Jaap Driessen <janjaapdriessen@gmail.com>
2015-02-13 10:35:35 +0100

    Back to development: 0.8.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.8.3'
+version = '0.8.4.dev0'
 
 long_description = (
     read('README.rst')
